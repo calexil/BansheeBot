f9e9f1b73637ef4866a83e280b680e1aa01dd49f
calexil <calexil@yahoo.com>
2018-01-04 16:15:01 -0500

    commands

--- bot.js
+++ bot.js
@@ -43,13 +43,13 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'hi') {
-    	message.reply('hello there, you can see my commands here: https://github.com/calexil/BansheeBot/blob/master/Commands');
+    	message.reply('hello there, you can see my commands here: https://github.com/calexil/BansheeBot/blob/master/Commands.md');
   	}
 });
 
 client.on('message', message => {
     if (message.content === 'commands') {
-    	message.reply('you can see my commands here: https://github.com/calexil/BansheeBot/blob/master/Commands');
+    	message.reply('you can see my commands here: https://github.com/calexil/BansheeBot/blob/master/Commands.md');
   	}
 });
 
