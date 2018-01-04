2f254e11993d70e75ec96648d30dcfd1e34eb4b3
calexil <calexil@yahoo.com>
2018-01-03 20:58:32 -0500

    make it work3

--- bot.js
+++ bot.js
@@ -17,6 +17,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'best girl?') {
+    	message.reply('Big Band :clappa:');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
