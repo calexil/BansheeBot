4feda3a6fb7768ee1ae0549c8d8be54a3d15bc28
calexil <calexil@yahoo.com>
2018-01-06 19:54:47 -0500

    make it work 6

--- bansheecheck.sh
+++ bansheecheck.sh
@@ -7,6 +7,13 @@ do
     title=$(banshee --query-title | sed 's/title: //')
     artist=$(banshee --query-artist | sed 's/artist: //')
     album=$(banshee --query-album | sed 's/album: //')
+    track="Calexil is listening to $title by $artist on $album"
     echo "Calexil is listening to $title by $artist on $album"
-    sleep 6
+    if $currentTrack != $track
+        $currentTrack = $track
+        curl --data "trackName=$currentTrack" https://bansheebot.herokuapp.com/endpoint
+    sleep 15
+then
+    sleep 5
+fi
 done
