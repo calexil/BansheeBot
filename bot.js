5870838415f216bdf3a1dd23f806559d3bdf562c
calexil <calexil@yahoo.com>
2018-01-07 19:41:49 -0500

    make it work 9.8

--- bot.js
+++ bot.js
@@ -8,14 +8,14 @@ const bodyParser = require('body-parser');
 
 // Message Array
 const responseObject = {
-  "test": "It worked!",
-  "best girl?": "Big Band.",
-  "best girl": "Big Band.",
-  "best grill": "Big Band.",
-  "best grill?": "Big Band.",
-  "hi": "Hello there, I'm a dumb bot written by @calexil#9270 and @Timberius#8180 , you can see my commands here: https://github.com/calexil/BansheeBot/blob/master/Commands.md",
-  "commands": "You can see my commands here: https://github.com/calexil/BansheeBot/blob/master/Commands.md",
-  "song": "Currently playing: https://wagnaria.xyz/now/calexil"
+    "test": "It worked!",
+    "best girl?": "Big Band.",
+    "best girl": "Big Band.",
+    "best grill": "Big Band.",
+    "best grill?": "Big Band.",
+    "hi": "Hello there, I'm a dumb bot written by @calexil#9270 and @Timberius#8180 , you can see my commands here: https://github.com/calexil/BansheeBot/blob/master/Commands.md",
+    "commands": "You can see my commands here: https://github.com/calexil/BansheeBot/blob/master/Commands.md",
+    "song": "Currently playing: https://wagnaria.xyz/now/calexil"
 };
 
 //const trackName = {console.log(`${trackName}`)
@@ -24,7 +24,9 @@ const responseObject = {
 // Call the web page with express
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Listen for the users local script to post the current track and log it to console
 app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`))
@@ -46,14 +48,14 @@ client.on('ready', () => {
 
 // Reply to keywords in the array
 client.on("message", (message) => {
-  if(responseObject[message.content]) {
-    message.channel.send(responseObject[message.content]);
-  }
+    if (responseObject[message.content]) {
+        message.channel.send(responseObject[message.content]);
+    }
 });
 
 // Ping server every 15 minutes to prevent web dyno from sleeping
 setInterval(() => {
- http.get('http://bansheebot.herokuapp.com');
+    http.get('http://bansheebot.herokuapp.com');
 }, 900000);
 
 // Do not change
