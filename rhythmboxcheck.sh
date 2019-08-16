220674061f6fd9c2a43065672e3c6d6d80ad5a6a
calexil <calexil@teknik.io>
2019-08-16 15:35:00 -0400

    added rhythmbox

--- rhythmboxcheck.sh
+++ rhythmboxcheck.sh
@@ -6,7 +6,7 @@ while :
 do
     title=$(rhythmbox-client --no-start --print-playing-format %tt | sed 's/title: //')
     artist=$(rhythmbox-client --no-start --print-playing-format %aa | sed 's/artist: //')
-    album=$(banshee --query-album | sed 's/album: //')
+    album=$(rhythmbox-client --no-start --print-playing-format %at | sed 's/album: //')
     track="Calexil is listening to $title by $artist on $album"
     echo $track
     if [[ $currentTrack = "$track" ]]
