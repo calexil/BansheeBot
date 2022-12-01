94216c97735867f68d8b2d27096abae15c2f54d3
calexil <calexil@teknik.io>
2022-12-01 18:33:27 -0500

    getting intents rigged up

--- bot.js
+++ bot.js
@@ -1,3 +1,6 @@
+// import Discord from 'discord.js'
+import { Client, GatewayIntentBits } from 'discord.js'
+
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
 const mySecret = `${process.env['BOT_TOKEN']}`;
