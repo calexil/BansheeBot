fcaada622fbe5d8705b9c211f0fb32e077152546
calexil <admin@calexil.com>
2024-07-12 16:21:29 -0400

    prevent onrender from spinning down the dyno

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const responseObject = {
 
 const inBotConfigs = {
     renderApp: "http://bansheebot.onrender.com", /* Server URL */
-    pingInterval: "900000", /*  */
+    pingInterval: "750000", /*  */
 };
 
 // Reply to regex regarding best girl.
